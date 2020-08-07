7a96818117d5256eab75f936478bbec9634c7edc
kkuczynski <krzysiek.kuczynski@icloud.com>
2020-08-07 14:01:30 +0200

    css xd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,16 +17,18 @@ export class AppComponent {
   changeNightMode() {
     if(this.nightMode.getNightMode()===1) {
       console.log('sun lowering, moon rising');
-      document.getElementById('sun').style.animationName = 'iconTransitionOff';
-      document.getElementById('moon').style.animationName = 'iconTransitionOn';
-      document.getElementById('bar').style.animationName = 'backgroundTransitionOff';
+      document.getElementById('sun').style.animationName = 'iconTransitionSunOff';
+      document.getElementById('moon').style.animationName = 'iconTransitionMoonOn';
+      // document.getElementById('bar').style.animationName = 'backgroundTransitionOff';
       document.getElementById('body').style.animationName = 'backgroundTransitionOff';
+      document.getElementById('music').style.animationName = 'iconTransitionMoonOff';    
     } else {
       console.log('sun rising, moon lowering');
-      document.getElementById('sun').style.animationName = 'iconTransitionOn';
-      document.getElementById('moon').style.animationName = 'iconTransitionOff';
-      document.getElementById('bar').style.animationName = 'backgroundTransitionOn';
+      document.getElementById('sun').style.animationName = 'iconTransitionSunOn';
+      document.getElementById('moon').style.animationName = 'iconTransitionMoonOff';
+      // document.getElementById('bar').style.animationName = 'backgroundTransitionOn';
       document.getElementById('body').style.animationName = 'backgroundTransitionOn';
+      document.getElementById('music').style.animationName = 'iconTransitionMoonOn';
     }
     this.nightMode.changeNightMode()
   }
