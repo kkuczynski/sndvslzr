89f8bc5be0e5a3aaff5f3bdc804f36ab00dc51dc
kkuczynski <krzysiek.kuczynski@icloud.com>
2020-08-07 16:43:09 +0200

    added sound playing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NightModeService } from './services/night-mode.service';
+import { PlaylistService } from './services/playlist.service';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,35 @@ import { NightModeService } from './services/night-mode.service';
 export class AppComponent {
   title = 'sndvslzr';
   private nightMode: NightModeService;
+  private playlistService: PlaylistService;
   constructor() {
     this.nightMode = new NightModeService;
+    this.playlistService = new PlaylistService;
   }
 
+  public onChange(fileList: FileList): void {
+    for (let i = 0; i < fileList.length; i++) {
+      this.playlistService.addToPlaylistSrc(URL.createObjectURL(fileList[i]));
+      let file = fileList[i];
+      console.log(file);
+      this.playlistService.addToPlaylist(file);
+      console.log(this.playlistService.getPlaylist());
+    }    
+  }
+
+  getPlaylistService() : string[] {
+    return this.playlistService.getPlaylist();
+  }
 
   changeNightMode() {
-    if(this.nightMode.getNightMode()===1) {
+    if (this.nightMode.getNightMode() === 1) {
       console.log('sun lowering, moon rising');
       document.getElementById('sun').style.animationName = 'iconTransitionSunOff';
       document.getElementById('moon').style.animationName = 'iconTransitionMoonOn';
       // document.getElementById('bar').style.animationName = 'backgroundTransitionOff';
       document.getElementById('body').style.animationName = 'backgroundTransitionOff';
-      document.getElementById('music').style.animationName = 'iconTransitionMoonOff';    
+      document.getElementById('music').style.animationName = 'iconTransitionMoonOff';
+      document.getElementById('palm').style.animationName = 'pngTransitionOff';
     } else {
       console.log('sun rising, moon lowering');
       document.getElementById('sun').style.animationName = 'iconTransitionSunOn';
@@ -29,12 +46,18 @@ export class AppComponent {
       // document.getElementById('bar').style.animationName = 'backgroundTransitionOn';
       document.getElementById('body').style.animationName = 'backgroundTransitionOn';
       document.getElementById('music').style.animationName = 'iconTransitionMoonOn';
+      document.getElementById('palm').style.animationName = 'pngTransitionOn';
     }
     this.nightMode.changeNightMode()
   }
 
-  getNightMode(): number {    
+  play(i: number) {
+    console.log('playing');
+    this.playlistService.play(i);
+  }
+
+  getNightMode(): number {
     return this.nightMode.getNightMode();
-  
+
   }
 }
--- src/app/services/playlist.service.ts
+++ src/app/services/playlist.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
+import { UrlObject } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
 
-  private playlist = [];
+  private playlist:string[] = [];
+  private playlistSrc: UrlObject[] = [];
   private currentSong;
   constructor() { }
 
-  addToPlaylist() {
+  addToPlaylist(file: File) {
+     this.playlist.push(file.name);
+     console.log(this.playlist);
+  }
 
+  addToPlaylistSrc(file) {
+    this.playlistSrc.push(file);
+    console.log(this.playlist);
+ }
+
+  getPlaylist(): string[] {
+    return this.playlist;
+  }
+
+  play(index: number) {
+    let sound = new Audio();
+    sound = document.getElementById('sound') as unknown as HTMLAudioElement;     
+    sound.src = this.playlistSrc[index] as unknown as string;    
+    sound.play;
+    
   }
 }
